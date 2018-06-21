e2cdf799d1032f701dd2221bdf87ce83aac829ee
miyoungvkim <miyoungvkim@gmail.com>
2018-06-21 14:25:09 +0900

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/creativeLab/samsung/mbf/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package creativeLab.samsung.mbf;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "creativeLab.samsung.mbf";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
